8ce05c506c6b7395973935bff28da8c8f4238db2
mdrayhanhowlader <rhowlader5314@gmail.com>
2024-04-18 17:43:14 +0800

    updated query by search and pagination and limit data

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,6 +1,8 @@
 import { AcademicSemester, PrismaClient } from "@prisma/client";
 import { IGenericResponse } from "../../../interfaces/common";
 import { paginationHelpers } from "../../../helpers/paginationHelper";
+import { IPaginationOptions } from "../../../interfaces/pagination";
+import { IAcademicSemesterFilterRequest } from "./academicSemester.interface";
 
 const prisma = new PrismaClient()
 
@@ -12,11 +14,28 @@ const insertIntoDB = async(academicSemesterData: AcademicSemester) : Promise<Aca
 }
 
 const getAllFromDB = async(
-    filters,
-    options
+    filters: IAcademicSemesterFilterRequest,
+    options: IPaginationOptions
 ):Promise<IGenericResponse<AcademicSemester[]>> => {
     const {page, limit, skip} = paginationHelpers.calculatePagination(options)
+    const {searchTerm, ...filterData} = filters;
     const result = await prisma.academicSemester.findMany({
+        where: {
+            OR: [
+                {
+                    title: {
+                        contains: searchTerm,
+                        mode: 'insensitive'
+                    }
+                },
+                {
+                    code: {
+                        contains: searchTerm,
+                        mode: 'insensitive'
+                    }
+                }
+            ]
+        },
         skip,
         take: limit
     });
